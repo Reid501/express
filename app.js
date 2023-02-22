6305344e7dfdd80eaaba0d1b7d3a58ae8cc106e7
reid501 <alexatrs@hotmail.co.uk>
2023-02-22 17:30:22 +0000

    Code changed into handlebars template engine

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
+const expressHbs = require("express-handlebars");
 
 const app = express();
 
-app.set("view engine", "pug");
+app.engine("handlebars", expressHbs());
+app.set("view engine", "handlebars");
 // Set directors for template, this is not needed if files is already views
 // app.set('views', 'views');
 
@@ -18,7 +20,7 @@ app.use("/admin", adminDta.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
 });
 
 app.listen(3000);
