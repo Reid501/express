4a1ab7ca3ddd72d1bbe03c1c7f96dc702774cfc4
reid501 <alexatrs@hotmail.co.uk>
2023-03-09 15:51:43 +0000

    models added

--- controllers/products.js
+++ controllers/products.js
@@ -1,19 +1,22 @@
-const products = [];
+const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
   res.render("add-product", { docTitle: "Shop", path: "/admin/add-product" });
 };
 
 exports.postAddProduct = (req, res) => {
-  products.push({ title: req.body.title });
+  const product = new Product(req.body.title);
+  product.save();
   res.redirect("/");
 };
 
 exports.getProducts = (req, res, next) => {
-  res.render("shop", {
-    prods: products,
-    docTitle: "Shop",
-    path: "/",
-    hasProducts: products.length > 0,
+  Product.fetchAll((products) => {
+    res.render("shop", {
+      prods: products,
+      docTitle: "Shop",
+      path: "/",
+      hasProducts: products.length > 0,
+    });
   });
 };
